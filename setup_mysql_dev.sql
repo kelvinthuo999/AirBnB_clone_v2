7584a42491c283d17f3311f88223f9f69527e0b1
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-21 05:50:10 -0800

    create an sql script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,23 +1,5 @@
-#!/bin/bash
-
-# MySQL server connection details
-MYSQL_USER="root"
-MYSQL_PASSWORD="your_mysql_root_password"
-MYSQL_HOST="localhost"
-
-# Database and user details
-DB_NAME="hbnb_dev_db"
-DB_USER="hbnb_dev"
-DB_PASSWORD="hbnb_dev_pwd"
-
-# Create the database
-mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}" -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-
-# Create the user
-mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}" -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-
-# Grant all privileges on the database to the user
-mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}" -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';"
-
-# Grant SELECT privilege on performance_schema database to the user
-mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}" -e "GRANT SELECT ON performance_schema.* TO '${DB_USER}'@'localhost';"
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
