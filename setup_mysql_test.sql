965670a93a85c34223a0b3ae0cc2e5d9a30e6d42
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-21 05:56:07 -0800

    created sql test file

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- Script to prepare a MySQL server for the project.
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema database to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
