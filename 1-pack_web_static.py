f7e5835cf654f07fb972148fccbc4c20f8d24c89
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-12-07 01:31:30 -0800

    create: script to fully deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,27 +16,12 @@ def do_pack():
         Archive path if successful, None otherwise
     """
     try:
-        # Create the "versions" folder if it doesn't exist
-        local("mkdir -p versions")
-
-        # Get the current date and time
-        now = datetime.now()
-        date_time = now.strftime("%Y%m%d%H%M%S")
-
-        # Create the archive path
-        archive_path = "versions/web_static_{}.tgz".format(date_time)
-
-        # Compress the contents of the web_static folder into the archive
-        local("tar -czvf {} web_static".format(archive_path))
-
-        # Return the archive path if successful
-        return archive_path
-    except Exception as e:
-        # Print an error message and return None if an exception occurs
-        print("Error: {}".format(str(e)))
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+    except:
         return None
-
-
-if __name__ == "__main__":
-    # Call the do_pack function when the script is executed
-    do_pack()
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,53 +3,46 @@
 Fabric script that distributes an archive to your web servers
 """
 
-from fabric.api import put, run, env
-from os.path import exists
-
+from fabric.api import *
+import os
+from datetime import datetime
 
 env.hosts = ['54.152.236.201', '54.175.134.3']
 env.user = 'ubuntu' 
 
+def do_pack():
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
+    try:
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+    except:
+        return None
+
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-
-    Args:
-        archive_path (str): Path to the archive to be deployed
-
-    Returns:
-        True if all operations have been done correctly, otherwise returns False
-    """
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
-
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        # Upload the archive to /tmp/ directory on the web server
         put(archive_path, '/tmp/')
-
-        # Extract the archive to /data/web_static/releases/<filename without extension>/
-        archive_filename = archive_path.split('/')[-1]
-        release_folder = '/data/web_static/releases/{}'.format(archive_filename.split('.')[0])
-        run('mkdir -p {}'.format(release_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
-
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_filename))
-
-        # Delete the existing symbolic link /data/web_static/current
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link to the new version
-        run('ln -s {} /data/web_static/current'.format(release_folder))
-
+        run('ln -s {} /data/web_static/current'.format(releases_path))
         print('New version deployed!')
         return True
-    except Exception as e:
-        print('Error: {}'.format(str(e)))
+    except:
         return False
-
-
-if __name__ == "__main__":
-    # Example usage: fab -f 2-do_deploy_web_static.py do_deploy:<path_to_archive>
-    pass
