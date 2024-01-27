b831832dc18d22393f741cf521336e8498167a55
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-01-26 20:55:30 -0800

    fix: pycodestyle on file 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,7 +5,9 @@ Script starts a Flask web application with two routes.
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -14,12 +16,14 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """
     Function handles the route ('/hbnb')
     """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
